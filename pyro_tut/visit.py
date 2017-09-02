2922661a83eb7b39d5dc3ba0ce060fcb5f7bb27d
Daniel <daniellsmarta@gmail.com>
2017-09-02 01:59:23 +0100

    Pyro and yield tutorials

--- pyro_tut/visit.py
+++ pyro_tut/visit.py
@@ -1,8 +1,12 @@
-# This is the code that runs this example.
-from warehouse import Warehouse
+# This is the code that visits the warehouse.
+import sys
+import Pyro4
+import Pyro4.util
 from person import Person
 
-warehouse = Warehouse()
+sys.excepthook = Pyro4.util.excepthook
+
+warehouse = Pyro4.Proxy("PYRONAME:example.warehouse")
 janet = Person("Janet")
 henry = Person("Henry")
 janet.visit(warehouse)
--- pyro_tut/warehouse.py
+++ pyro_tut/warehouse.py
@@ -1,5 +1,9 @@
 from __future__ import print_function
+import Pyro4
 
+
+@Pyro4.expose
+@Pyro4.behavior(instance_mode="single")
 class Warehouse(object):
     def __init__(self):
         self.contents = ["chair", "bike", "flashlight", "laptop", "couch"]
@@ -14,3 +18,14 @@ class Warehouse(object):
     def store(self, name, item):
         self.contents.append(item)
         print("{0} stored the {1}.".format(name, item))
+
+
+def main():
+    Pyro4.Daemon.serveSimple(
+            {
+                Warehouse: "example.warehouse"
+            },
+            ns = True)
+
+if __name__=="__main__":
+    main()
